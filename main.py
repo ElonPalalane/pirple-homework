dd40da7579108d0481a15e4377673c5c271bff63
Elon Palalane <46702193+ElonPalalane@users.noreply.github.com>
2019-01-15 11:41:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Homework #1: Variables
+Task : Think about a song of you liking and think about all the attributes that you could use to describe that song. These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can
+Now crate variables for every attribute and print them out.
+"""
+
+#Attributes
+Artist = "Vincent Fenton"
+ProfessionalName = "French Kiwi Juice"
+StageName = "FKJ"
+Song = "Why are there Boundaries"
+Album = "French Kiwi juice"
+Genre = "Rhythm and Blues"
+YearReleased = 2017
+DurationSeconds = 182
+
+#Printing the variables
+print(Artist)
+print(ProfessionalName)
+print(StageName)
+print(Song)
+print(Album)
+print(Genre)
+print(YearReleased)
+print(DurationSeconds)
